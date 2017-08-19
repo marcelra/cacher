f7e7ec1ece859311cd24899fa2d16ace0186c6e1
Marcel Raas <marcel.raas@gmail.com>
2017-08-19 18:51:08 +0200

    Add run script.

---
+++ tests/run.sh
@@ -0,0 +1,12 @@
+BASEDIR=$(dirname "$0")
+echo $BASEDIR
+PATHTOBIN=`pwd`/$BASEDIR/../bin
+LOGFILE=$PATHTOBIN/../log/server.out
+touch $LOGFILE
+echo $PATHTOBIN
+xterm -e tail -f $PATHTOBIN/../log/server.out &
+$PATHTOBIN/server > $LOGFILE &
+$PATHTOBIN/client
+kill -15 %1
+kill -15 %2
+exit
